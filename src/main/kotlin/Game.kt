da7b60ac0a53a4954e974863b95d533fb438516b
Роман Кузнецов <roman_k_a@mail.ru>
2020-10-14 11:37:58 +0300

    log4j2 setup+

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,6 +1,6 @@
 
 fun main() {
-  System.setProperty("log4j.configurationFile", "E:\\JavaProject\\Engine\\resources\\log4j2.xml") // путь к конфигу log4j2
+  System.setProperty("log4j.configurationFile", ".\\resources\\log4j2.xml") // путь к конфигу log4j2
   val engine = Engine()
   engine.run()
 }
